90796506b6326950b2fc77b5114bdfd0d2cddc4a
aliii <al1yxhfiles@gmail.com>
2026-06-14 13:53:10 +0800

    Update IApplicantAccountRepository.cs

--- HRApplicantSystem.Data/Repositories/IApplicantAccountRepository.cs
+++ HRApplicantSystem.Data/Repositories/IApplicantAccountRepository.cs
@@ -7,7 +7,8 @@ public interface IApplicantAccountRepository
     Task<IEnumerable<ApplicantAccount>> GetAllAsync();
     Task<ApplicantAccount?> GetByIdAsync(string id);
     Task<ApplicantAccount?> GetByEmailAsync(string email);
-    Task<bool> CreateAsync(ApplicantAccount account);
+    Task<bool> CreateAsync(ApplicantDocument document);
     Task<bool> UpdateAsync(ApplicantAccount account);
-    Task<bool> DeleteAsync(string id);
+    Task<bool> DeleteAsync(string documentId);
 }
+    
