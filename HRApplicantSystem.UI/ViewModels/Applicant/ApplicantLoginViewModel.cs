45bb09c60f00344fba5f80cda56c1ece652af44e
Rayven Malabanan <rayven.malabanan.7@gmail.com>
2026-06-14 20:07:40 +0800

    Fix applicant dashboard navigation and back button bindings

--- HRApplicantSystem.UI/ViewModels/Applicant/ApplicantLoginViewModel.cs
+++ HRApplicantSystem.UI/ViewModels/Applicant/ApplicantLoginViewModel.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Threading.Tasks;
-using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using HRApplicantSystem.Data;
